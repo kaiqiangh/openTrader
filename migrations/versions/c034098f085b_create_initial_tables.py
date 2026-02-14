cdc69cfcd22bec9020ab40ee55378900ae3aa7b9
Kai <kaiqianghuang666@gmail.com>
2026-02-14 11:21:12 +0000

    feat: create news schema and initial tables migration

---
+++ migrations/versions/c034098f085b_create_initial_tables.py
@@ -0,0 +1,26 @@
+"""create_initial_tables
+
+Revision ID: c034098f085b
+Revises: 20260214_0005
+Create Date: 2026-02-14 11:20:06.094805
+"""
+
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c034098f085b'
+down_revision = '20260214_0005'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
