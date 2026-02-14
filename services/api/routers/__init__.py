2c03177851de5040d26e2f179e39c02249f6e9bf
Kai <kaiqianghuang666@gmail.com>
2026-02-14 17:56:07 +0000

    feat: add api route dashboard, governance and replay

--- services/api/routers/__init__.py
+++ services/api/routers/__init__.py
@@ -1,5 +1,15 @@
 from services.api.routers.control import router as control_router
+from services.api.routers.dashboard import router as dashboard_router
+from services.api.routers.governance import router as governance_router
 from services.api.routers.ops import router as ops_router
+from services.api.routers.replay import router as replay_router
 from services.api.routers.system import router as system_router
 
-__all__ = ["system_router", "control_router", "ops_router"]
+__all__ = [
+    "system_router",
+    "control_router",
+    "ops_router",
+    "governance_router",
+    "replay_router",
+    "dashboard_router",
+]
